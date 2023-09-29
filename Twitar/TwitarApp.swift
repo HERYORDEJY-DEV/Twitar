d2575074d284c8beff8197678f41510c605dfbe6
HERYORDEJY <heryordejy.dev@gmail.com>
2023-09-29 18:05:29 +0100

    Initial Commit

---
+++ Twitar/TwitarApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitarApp.swift
+//  Twitar
+//
+//  Created by  OYEBODE YUSUF AYODEJI on 9/29/23.
+//
+
+import SwiftUI
+
+@main
+struct TwitarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
